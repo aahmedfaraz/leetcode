02ac46e1d28b81d11932ad4026e1e96a6ec95a6d
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-02-02 16:52:32 +0500

    Time: 554 ms (43.98%), Space: 70.7 MB (49.06%) - LeetHub

--- 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.py
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.py
@@ -9,26 +9,32 @@ class Solution:
             graph[u].append((v, w))
             graph[v].append((u, 2 * w))
         
-        # Dijkstra's algorithm
+        # Dijkstra algorithm
         pq = [(0, 0)]  # (cost, node)
-        min_costs = {0: 0}
+        min_costs = {
+            0: 0
+        }
         
         while pq:
             cost, u = heapq.heappop(pq)
             
+            # if we are already have min cost of this node
             if cost > min_costs.get(u, float('inf')):
                 continue
             
+            # Base case - found target node
             if u == n - 1:
                 return cost
-                
+            
+            # check cost with all neighbours
             for v, weight in graph[u]:
                 new_cost = cost + weight
+                # found less cost with a neighbour
                 if new_cost < min_costs.get(v, float('inf')):
-                    min_costs[v] = new_cost
-                    heapq.heappush(pq, (new_cost, v))
+                    min_costs[v] = new_cost # update min cost
+                    heapq.heappush(pq, (new_cost, v)) # push this node with updated cost in heap
                     
         return -1
 
-# Time complexity = O(E log V)
-# Space complexity = O(V + E)
+# Time complexity = O(E log V) = O(n log n)
+# Space complexity = O(V + E) = O(n)
