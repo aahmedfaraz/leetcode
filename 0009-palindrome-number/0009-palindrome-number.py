9ced42fca3b5ca1749e79dcdab8b512933a57bc7
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-02-04 05:41:28 +0500

    Time: 4 ms (80.38%), Space: 19.3 MB (28.83%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,6 +1,17 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        num = str(x)
-        return num == num[::-1]
+        nums = str(x)
+
+        if len(nums) == 1: return True
+        if len(nums) == 2 and nums[0] == nums[1]: return True
+        
+        left, right = 0, len(nums) - 1
+        while left < right:
+            if nums[left] == nums[right]:
+                left += 1
+                right -= 1
+                continue
+            return False
+        return True
 # Time complexity = O(n) due to reverse
 # Space complexity = O(1)
