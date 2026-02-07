12b69c2acd0dade6b43981790e2c497c0291100c
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-02-07 11:19:08 +0500

    Time: 159 ms (79.45%), Space: 20.1 MB (55.07%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,40 +1,12 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        # thought process
-        # 0 1 2 3 4 5 6 7 8 9 10 11
-        # a a b a b a a |b| b b a  a
-        #                i
+        b_count = 0
+        deletions = 0
 
-        # n = 12, a = 7, b = 5
+        for ch in s:
+            if ch == "b":
+                b_count += 1
+            else:
+                deletions = min(deletions + 1, b_count)
 
-        # get totals
-        n = len(s)
-        total_a = s.count("a")
-        total_b = n - total_a
-
-        # loop through all characters, 
-        # considering each character as the border between left and right side
-        curr_a_count = 0
-        min_deletion = float('inf')
-        for i in range(n):
-            # get totals on left and right
-            a_on_left = curr_a_count
-            a_on_right = total_a - a_on_left
-            b_on_left = i - a_on_left
-            b_on_right = total_b - b_on_left
-
-            # make sure border character is excluded
-            a_on_right -= 1 if s[i] == "a" else 0
-            b_on_right -= 1 if s[i] == "b" else 0
-
-            # calculate deletion required as per current border
-            curr_deletion = b_on_left + a_on_right
-
-            # track the min deletion count
-            min_deletion = min(min_deletion, curr_deletion)
-
-            # keep tracking count of "a" for next border
-            curr_a_count += 1 if s[i] == "a" else 0
-
-        # return min deletion
-        return min_deletion
+        return deletions
