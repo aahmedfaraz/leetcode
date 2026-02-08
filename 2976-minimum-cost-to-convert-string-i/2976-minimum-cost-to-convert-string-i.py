7375fa0565bf7baf5a949fb75fc28a0a4b2cee5e
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-02-08 10:49:26 +0500

    Time: 734 ms (55.37%), Space: 20.9 MB (58.88%) - LeetHub

---
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+
+class Solution:
+    def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int: 
+        # we need minimum cost to convert each letter from "source" to "target"
+        # means we need shortest path from each letter to all other letters
+        # like,
+        # a -> b, c, d, ..., z
+        # b -> a, c, d, ..., z
+        # c -> a, b, d, ..., z
+        # .
+        # .
+        # z -> a, b, c, ..., y
+
+        # sounds like floyd warshal algorithm
+
+        # initialize 2d array to store shortest path from each letter to 26 letters
+        INF = float('inf')
+        dist = [[INF] * 26 for _ in range(26)]
+
+        # distance from a letter to itself is 0
+        for i in range(26):
+            dist[i][i] = 0
+
+        # enter distance of direct connected nodes
+        for i in range(len(original)):
+            from_letter = ord(original[i]) - ord('a')
+            to_letter = ord(changed[i]) - ord('a')
+            dist[from_letter][to_letter] = min(dist[from_letter][to_letter], cost[i])
+
+        # floyd warshall algorithm
+        for k in range(26):
+            for i in range(26):
+                for j in range(26):
+                    if dist[i][k] + dist[k][j] < dist[i][j]:
+                        dist[i][j] = dist[i][k] + dist[k][j]
+        
+        # calculate final cost
+        res = 0
+        for i in range(len(source)):
+            from_letter = ord(source[i]) - ord('a')
+            to_letter = ord(target[i]) - ord('a')
+            if dist[from_letter][to_letter] == INF:
+                return -1 # impossible transformation
+            res += dist[from_letter][to_letter]
+
+        return res
