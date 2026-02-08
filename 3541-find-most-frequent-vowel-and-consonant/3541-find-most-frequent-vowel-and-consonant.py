7d0710958e4c2e51da1a5d0827937ae8e0b7495a
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-02-08 14:30:14 +0500

    Time: 0 ms (100%), Space: 19.2 MB (57.73%) - LeetHub

--- 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.py
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.py
@@ -39,34 +39,3 @@ class Solution:
         )
 
         return max_vowel_freq + max_consonants_freq
-
-
-
-
-
-#         frequencies = [0 for _ in range(26)]
-
-#         for i in range(len(s)):
-#             letter = ord(s[i]) - ord('a') # character to index. 'a' means save at 0 index
-#             frequencies[letter] += 1
-
-#         vowels = ['a', 'e', 'i', 'o', 'u']
-#         max_vowel_freq = 0
-#         max_consonant_freq = 0
-
-#         for i in range(26):
-#             char = chr(i + ord('a')) # index to character. i=0 means 'a'
-#             if char in vowels:
-#                 max_vowel_freq = max(max_vowel_freq, frequencies[i])
-#             else:
-#                 max_consonant_freq = max(max_consonant_freq, frequencies[i])
-
-#         return max_vowel_freq + max_consonant_freq
-
-# # time complexity
-# # Frequencies loop = O(n)
-# # Calculate Max Freq. = O(26) = O(1)
-# # Overall = O(n) + O(26) = O(n)
-
-# # space complexity
-# # Array saving frequencies = O(26) = O(1)
