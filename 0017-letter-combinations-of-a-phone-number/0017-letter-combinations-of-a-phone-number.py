e45f35562045d9966990334aa68b0585bd39e646
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-02-17 18:39:59 +0500

    Time: 0 ms (100%), Space: 19.4 MB (57.09%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,5 +1,6 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        # space complexity = 4^n
         data = {
             '2': ['a', 'b', 'c'],
             '3': ['d', 'e', 'f'],
@@ -11,15 +12,17 @@ class Solution:
             '9': ['w', 'x', 'y', 'z'],
         }
 
-        combs = data[digits[0]] # ['a', 'b', 'c']
+        combs = [""]
 
-        for i, num in enumerate(digits): # num = '2' to '9'
-            if i == 0: continue
+        for num in digits: # can run upto 4 times since digits length <= 4
             new_arr = []
             alphabets = data[num] # ['d', 'e', 'f']
-            for prev_comb in combs: # prev_comb = 'a' then 'b' then 'c'
-                for char in alphabets: # char = 'd' then 'e' then 'f'
+            for prev_comb in combs: # prev_comb = 'a' then 'b' then 'c' - can run upto 4^n times
+                for char in alphabets: # char = 'd' then 'e' then 'f' - can run upto 4 times
                     new_arr.append(prev_comb + char)
             combs = new_arr
 
         return combs
+        
+# Time complexity = 4^n, where n can be max 4 so, 4^4, which is constant due to constraints
+# Space complexity = 4^n, where n is 8 numbers on phone so, 4^7, which is constant again
