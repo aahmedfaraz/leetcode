2b8077deca4e431bc2cdf5161c8a33f26f8d7f08
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-02-18 14:10:28 +0500

    Time: 51 ms (20.69%), Space: 19.3 MB (27.87%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        binary = bin(n)[2:]
+        zeros = '0' * (32 - len(binary))
+        rev = binary[::-1] + zeros
+        num = int(rev, 2)
+        return num
