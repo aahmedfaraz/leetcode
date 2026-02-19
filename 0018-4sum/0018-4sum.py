4e52d7e5c21e952268da304b807b024b5e68232d
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-02-19 14:03:09 +0500

    Time: 555 ms (15.8%), Space: 19.3 MB (67.07%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -12,17 +12,11 @@ class Solution:
         nums.sort()
         quads = set()
 
-        # print(nums)
-
         for i in range(n-3):
-            # print('i', i, nums[i])
             for j in range(i+1, n-2):
-                # print('j', j, nums[j])
                 k, l = j+1, n-1
                 while k < l:
-                    # print('k, l', k, l, nums[k], nums[l])
                     four_sum = nums[i] + nums[j] + nums[k] + nums[l]
-                    # print('4sum', four_sum)
                     if four_sum == target:
                         quads.add((nums[i], nums[j], nums[k], nums[l]))
                         l -= 1
@@ -32,14 +26,3 @@ class Solution:
                     else:
                         k += 1
         return list(quads)
-
-
-#  i        j     k  l
-#  0    1   2  3  4  5
-# [-4, -1, -1, 0, 1, 2]     target = -1, n=6, i=6-4=2, j=6-3=3
-
-# sum = -4, -1, -1, 2 = -4
-# sum = -4, -1, 0, 2 = -3
-# sum = -4, -1, 1, 2 = -2
-# sum = -4, -1, 0, 2 = -3
-# sum = -4, -1, 1, 2 = -2
