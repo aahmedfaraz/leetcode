982ec6880c555fd54229674014d1f7c5fe6abf3d
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-02-20 22:47:00 +0500

    Time: 1170 ms (13.75%), Space: 19.4 MB (46.72%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,12 @@
+from math import floor, pow
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2 : return x
+
+        i = 1
+        while (i*i) <= x:
+            i += 1
+        
+        return i - 1
+
