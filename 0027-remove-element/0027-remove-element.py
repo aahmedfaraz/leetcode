e0f95d9154c1fdf08cdb8f158d832d9eb2b2b7ba
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-02-21 23:08:17 +0500

    Time: 0 ms (100%), Space: 19.4 MB (16.91%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,7 +1,7 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0 
-
+        k = 0  # position to place next valid element
+        
         for i in range(len(nums)):
             if nums[i] != val:
                 nums[k] = nums[i]
