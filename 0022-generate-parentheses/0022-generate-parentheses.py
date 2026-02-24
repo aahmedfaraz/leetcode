07b1639c3d1cbd8094e7198b5e8ed61858c23959
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-02-24 05:51:24 +0500

    Time: 3 ms (31.05%), Space: 19.6 MB (31.67%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        combs = []
+
+        def add_combination(prev_comb: str, new_char: str, opening: int, closing: int):
+            if opening < closing or opening > n or closing > n: return
+            new_comb = prev_comb + new_char
+            if opening == closing and opening == n:
+                combs.append(new_comb)
+            add_combination(new_comb, "(", opening + 1, closing)
+            add_combination(new_comb, ")", opening, closing + 1)
+
+        add_combination("", "(", 1, 0)
+        add_combination("", ")", 0, 1)
+
+        return combs
