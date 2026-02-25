0ee04f2f490770a878a34367f1fbb1184b2d343c
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-02-25 23:46:09 +0500

    Time: 0 ms (100%), Space: 19.4 MB (45.89%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        total = numBottles # 12
+
+        while numBottles >= numExchange: # 3 >= 3
+            remainingEmpty = numBottles % numExchange # 0
+            newFilled = numBottles // numExchange # 3
+            total += newFilled # 12
+            numBottles = newFilled + remainingEmpty # 3 + 0 = 3
+
+        return total
