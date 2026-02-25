adbeb5436f39e433ec003f1d925ea5f25eb92972
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-02-25 23:34:11 +0500

    Time: 5 ms (48.67%), Space: 19.6 MB (23.51%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -0,0 +1,14 @@
+import heapq
+
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        heap = []
+        for num in arr:
+            total1s = bin(num).count('1')
+            heapq.heappush(heap, (total1s, num))
+
+        res = []
+        while heap:
+            _, num = heapq.heappop(heap)
+            res.append(num)
+        return res
