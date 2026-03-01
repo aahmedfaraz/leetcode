75dd6f819ba1516e7c9c0078066513966afed2bf
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-03-01 19:29:13 +0500

    Time: 0 ms (100%), Space: 19.4 MB (26.55%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,23 +3,17 @@ from typing import List
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         n = len(nums)
-        
-        # Step 1: find first decreasing index from right
         i = n - 2
         while i >= 0 and nums[i] >= nums[i + 1]:
             i -= 1
         
-        # Step 2: if found
         if i >= 0:
-            # Step 3: find just larger number
             j = n - 1
             while nums[j] <= nums[i]:
                 j -= 1
             
-            # Step 4: swap
             nums[i], nums[j] = nums[j], nums[i]
         
-        # Step 5: reverse remaining part
         left, right = i + 1, n - 1
         while left < right:
             nums[left], nums[right] = nums[right], nums[left]
