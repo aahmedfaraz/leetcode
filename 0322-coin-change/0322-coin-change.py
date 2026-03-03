5c0cdab4a85c68a363e81b7929631df52d759479
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-03-03 06:34:32 +0500

    Time: 457 ms (81.95%), Space: 19.6 MB (88.38%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -5,9 +5,13 @@ class Solution:
         dp = [float('inf')] * (amount+1)
         dp[0] = 0
 
-        for i in range(1, amount+1):
-            for coin in coins:
+        for i in range(1, amount+1): # runs n times
+            for coin in coins: # runs m times
                 if coin <= i:
                     dp[i] = min(dp[i], dp[i-coin] + 1)
 
         return -1 if dp[amount] == float('inf') else dp[amount]
+
+# Time = O(n x m)
+# Space = O(n+1) size of dp list so, O(n)
+# Where n = amount, m = no. of coins
