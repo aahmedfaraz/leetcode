254a7f18d80029e91d89b9b884a0d4c920b6ff78
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-03-19 15:23:25 +0500

    Time: 0 ms (100%), Space: 19.1 MB (99.64%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,19 +1,18 @@
 from collections import Counter
-
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         counts = Counter(s)
         length = 0
         odd_found = False
 
-        for c in counts:
-            if counts[c] % 2 == 0:
-                length += counts[c]         # use all if even
+        for char in counts:
+            if counts[char] % 2 == 0:
+                length += counts[char]
             else:
-                length += counts[c] - 1    # use the even part
-                odd_found = True           # can place 1 odd in center
-
+                length += counts[char] - 1
+                odd_found = True
+        
         if odd_found:
-            length += 1                    # add 1 for center
+            length += 1
 
         return length
