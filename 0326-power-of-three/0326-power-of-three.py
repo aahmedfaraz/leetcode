30d138f2171c1fa9249b79fb471441e43d2cbcc6
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-03-22 00:19:42 +0500

    Time: 7 ms (68.39%), Space: 19.1 MB (99.19%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -1,9 +1,8 @@
 import math
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        if n <= 0: return False
-        res = math.log(n, 3)
-        rounded = round(res)
-        diff = (rounded - res) if rounded >= res else res - rounded
-        print(res, rounded, diff)
-        return diff >= 0 and diff <= 1e-12
+        if n < 3: return n == 1
+        start = 3
+        while start < n:
+            start *= 3
+        return start == n
