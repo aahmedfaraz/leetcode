f58dbffd20504156302dd576b43709b322a4b70a
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-03-22 11:37:40 +0500

    Time: 2 ms (8.62%), Space: 19.6 MB (8.15%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.py
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -1,9 +1,12 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        bx = bin(x)[2:].zfill(32)
-        by = bin(y)[2:].zfill(32)
+        bx = bin(x)[2:]
+        by = bin(y)[2:]
+        limit = min(max(len(bx), len(by)), 32)
+        bx = bx.zfill(limit)
+        by = by.zfill(limit)
         count = 0
-        for i in range(32):
+        for i in range(limit):
             if bx[i] != by[i]:
                 count += 1
         return count
