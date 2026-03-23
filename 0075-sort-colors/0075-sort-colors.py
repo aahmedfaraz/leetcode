3776fbdd122bdeea056953ae68d1fe176ef7d002
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-03-23 22:47:45 +0500

    Time: 0 ms (100%), Space: 19.1 MB (95.96%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,30 +1,17 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        n = len(nums)
-        i, j, k = 0, n-1, 0
+        left, mid, right = 0, 0, len(nums) - 1
 
-        # place i and j to correct positions
-        while i < n and nums[i] == 0:
-            i += 1
-        while j > -1 and nums[j] == 2:
-            j -= 1
-        
-        k = i
-        
-        # use k to traverse whole array and swap accordingly
-        while i < j and k <= j:
-            if nums[k] == 0:
-                nums[k], nums[i] = nums[i], nums[k]
-                i += 1
-                while i < n and nums[i] == 0:
-                    i += 1
-            elif nums[k] == 2:
-                nums[k], nums[j] = nums[j], nums[k]
-                j -= 1
-                while j > -1 and nums[j] == 2:
-                    j -= 1
-                while i < n and nums[i] == 0:
-                    i += 1
-                k = i
+        while mid <= right:
+            if nums[mid] == 0:
+                nums[left], nums[mid] = nums[mid], nums[left]
+                left += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
             else:
-                k += 1
+                nums[right], nums[mid] = nums[mid], nums[right]
+                right -= 1
+
+# Time = O(n)
+# Space = O(1)
