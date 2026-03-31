d12c2b0e9afb633f5886d0b1acdea37b477e15c5
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-03-31 22:38:51 +0500

    Time: 0 ms (100%), Space: 19.1 MB (96.27%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -3,17 +3,21 @@ class Solution:
         if not s or s[0] == '0':
             return 0
         
-        prev2, prev1 = 1, 1
+        prev2 = 1  # dp[i-2]
+        prev1 = 1  # dp[i-1]
         
         for i in range(1, len(s)):
             curr = 0
             
+            # single digit
             if s[i] != '0':
                 curr += prev1
             
+            # two digits
             if 10 <= int(s[i-1:i+1]) <= 26:
                 curr += prev2
             
-            prev2, prev1 = prev1, curr
+            prev2 = prev1
+            prev1 = curr
         
         return prev1
