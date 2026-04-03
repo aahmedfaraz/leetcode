8bb2472337bfbda5795043db2e7a54d52698e690
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-04-04 02:00:14 +0500

    Time: 0 ms (100%), Space: 19.4 MB (43.31%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,16 +1,5 @@
+from collections import Counter
+
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        data = {}
-        for char in s:
-            if char not in data:
-                data[char] = 1
-            else:
-                data[char] += 1
-
-        for char in t:
-            if char not in data or data[char] == 0:
-                return char
-            elif data[char]:
-                data[char] -= 1
-        
-        return ""
+        return list(Counter(t) - Counter(s))[0]
