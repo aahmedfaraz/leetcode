20e46329b68697798658eb0b206d1980424b6d35
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-04-04 20:49:59 +0500

    Time: 347 ms (18.83%), Space: 27.7 MB (70.78%) - LeetHub

---
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.py
@@ -0,0 +1,14 @@
+class Solution:
+    def decodeCiphertext(self, encodedText: str, rows: int) -> str:
+        cols = len(encodedText) // rows
+        originalText = ""
+        spaces = 0
+        for col in range(cols):
+            while col < len(encodedText):
+                if encodedText[col] == " ":
+                    spaces += 1
+                else:
+                    originalText += f'{' '*spaces}{encodedText[col]}'
+                    spaces = 0
+                col += (cols + 1)
+        return originalText
