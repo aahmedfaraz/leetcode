c9375626fa5b074f3c2884f16619118fad71c348
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-04-05 18:32:13 +0500

    Time: 5 ms (31.54%), Space: 19.2 MB (85.71%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.py
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.py
@@ -2,11 +2,15 @@ import copy
 class Solution:
     def areSimilar(self, mat: List[List[int]], k: int) -> bool:
         rows, cols = len(mat), len(mat[0])
-        k %= cols
+        for r in range(rows):
+            for c in range(cols):
+                if r % 2 == 0:  # left shift
+                    if mat[r][c] != mat[r][(c + k) % cols]:
+                        return False
+                else:  # right shift
+                    if mat[r][c] != mat[r][(c - k) % cols]:
+                        return False
+        return True
 
-        new = []
-
-        for row in range(rows):
-            new.append(mat[row][k:] + mat[row][:k])
-
-        return new == mat
+# Time = O(rows  x cols)
+# Space = O(1)
