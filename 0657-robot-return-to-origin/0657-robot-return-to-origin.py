70d25da73fefc54c3dfff655aced7e85b49e834a
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-04-05 18:06:02 +0500

    Time: 0 ms (100%), Space: 19.3 MB (33.52%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -1,14 +1,5 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        start = [0, 0]
-        end = [0, 0]
-        for move in moves:
-            if move == 'U':
-                start[1] += 1
-            elif move == 'D':
-                start[1] -= 1
-            elif move == 'L':
-                start[0] -= 1
-            else:
-                start[0] += 1
-        return start == end
+        x = moves.count('R') - moves.count('L')
+        y = moves.count('U') - moves.count('D')
+        return x == 0 and y == 0
