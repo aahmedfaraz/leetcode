ff793c17968d77d9b114e0d2e2bb0802cb26d337
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-04-08 04:38:03 +0500

    Time: 0 ms (100%), Space: 19.3 MB (71.11%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,18 +1,14 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         visited = set()
-        
         while True:
             if n == 1:
                 return True
-            
-            if n in visited:  # found loop
+            if n in visited: # found loop
                 return False
-            
             visited.add(n)
-            
+            nums = list(str(n))
             res = 0
-            for digit in str(n):
-                res += int(digit) ** 2
-            
+            for num in nums:
+                res += int(num)**2
             n = res
