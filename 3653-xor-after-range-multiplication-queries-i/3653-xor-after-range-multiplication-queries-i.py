057aa71a2f84be512b92ee6c8029f50adffd5985
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-04-08 17:48:26 +0500

    Time: 1662 ms (42.9%), Space: 20.1 MB (48.61%) - LeetHub

---
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
+        for l, r, k, v in queries:
+            idx = l
+            while idx <= r:
+                nums[idx] = (nums[idx] * v) % (10**9 + 7)
+                idx += k
+        res = 0
+        for num in nums:
+            res ^= int(num)
+        return res
