96cd56bfae093ca54762e0014094034924973d7e
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-04-16 11:35:04 +0500

    Time: 0 ms (100%), Space: 19.4 MB (53.18%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -1,19 +1,10 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        def printmat(mat):
-            for row in mat:
-                print("\t".join(map(str, row)))
-            print('-----------')
-
         mat = [([0] * n) for _ in range(n)]
-
-        # printmat(mat)
-
         layers = (n // 2) + 1
         i = 1
 
         for l in range(layers):
-            # print('LAYER', l)
             # mention current box ranges as per layer
             start, end = l, l+n-1
 
@@ -23,7 +14,6 @@ class Solution:
                 mat[start][col] = i
                 i += 1
                 col += 1
-            # printmat(mat)
 
             # travel last col
             row = start + 1
@@ -31,7 +21,6 @@ class Solution:
                 mat[row][end] = i
                 i += 1
                 row += 1
-            # printmat(mat)
             
             if start != end:
                 # travel last row - reversed
@@ -40,7 +29,6 @@ class Solution:
                     mat[end][col] = i
                     i += 1
                     col -= 1
-                # printmat(mat)
             
             # travel first col - reversed
             row = end-1
@@ -48,9 +36,11 @@ class Solution:
                 mat[row][start] = i
                 i += 1
                 row -= 1
-            # printmat(mat)
             
             # update n for next smaller box
             n -= 2
 
         return mat
+
+# Time = O(n^2)
+# Space = O(n^2) and O(1) without Output space
