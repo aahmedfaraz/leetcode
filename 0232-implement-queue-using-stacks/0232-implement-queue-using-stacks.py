d9b804125345198576b342fb9edd8a1ef9836c4b
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-05-21 21:14:18 +0500

    Time: 0 ms (100%), Space: 19.5 MB (49.08%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -0,0 +1,26 @@
+class MyQueue:
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        self._move()
+
+        return self.stack2.pop()
+
+    def peek(self) -> int:
+        self._move()
+
+        return self.stack2[-1]
+
+    def empty(self) -> bool:
+        return len(self.stack1) == 0 and len(self.stack2) == 0
+
+    def _move(self):
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
