f70036aa16a02c9a788123713759bf78f3eb261b
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-06-02 21:41:09 +0500

    Time: 19 ms (96.98%), Space: 19.3 MB (66.38%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -1,42 +1,24 @@
-from collections import defaultdict
-from math import gcd
-
 class Solution:
-    def maxPoints(self, points):
+    def maxPoints(self, points: List[List[int]]) -> int:
         n = len(points)
-
-        if n <= 2:
-            return n
-
-        ans = 0
-
-        for i in range(n):
-            slopes = defaultdict(int)
-
-            x1, y1 = points[i]
-
-            for j in range(i + 1, n):
-                x2, y2 = points[j]
-
-                dx = x2 - x1
-                dy = y2 - y1
-
-                g = gcd(dx, dy)
-
-                dx //= g
-                dy //= g
-
-                # normalize sign
-                if dx < 0:
-                    dx *= -1
-                    dy *= -1
-                elif dx == 0:
-                    dy = 1
-                elif dy == 0:
-                    dx = 1
-
-                slopes[(dy, dx)] += 1
-
-            ans = max(ans, max(slopes.values(), default=0) + 1)
-
-        return ans
+        if n < 2: return n
+        maxpoints = 1
+        for i in range(n-1):
+            point1 = points[i]
+            x1, y1 = point1[0], point1[1]
+            slopes = {}
+            # print("point1", point1)
+            for j in range(i+1, n):
+                point2 = points[j]
+                x2, y2 = point2[0], point2[1]
+                slope = float('inf')
+                if x1 != x2:
+                    slope = (y2-y1) / (x2-x1)
+                if slope in slopes:
+                    slopes[slope] += 1
+                else:
+                    slopes[slope] = 2
+                maxpoints = max(maxpoints, slopes[slope])
+                # print("-> point2", point2, " slopes ", slopes)
+            
+        return maxpoints
