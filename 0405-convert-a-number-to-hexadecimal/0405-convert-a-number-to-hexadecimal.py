223dc6450789900769b60d4d33d148aaa73cf582
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-06-15 23:15:49 +0500

    Time: 0 ms (100%), Space: 19.5 MB (6.71%) - LeetHub

--- 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.py
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.py
@@ -2,16 +2,33 @@ class Solution:
     def toHex(self, num: int) -> str:
         if num == 0:
             return "0"
-        
-        hex_chars = "0123456789abcdef"
-        result = []
-        
-        # treat number as 32-bit unsigned
-        num &= 0xffffffff
-        
-        while num > 0:
-            digit = num & 15  # last 4 bits
-            result.append(hex_chars[digit])
-            num >>= 4
-        
-        return "".join(reversed(result))
+
+        if num < 0:
+            num &= 0xFFFFFFFF
+
+        vals = {
+            0: '0',
+            1: '1',
+            2: '2',
+            3: '3',
+            4: '4',
+            5: '5',
+            6: '6',
+            7: '7',
+            8: '8',
+            9: '9',
+            10: 'a',
+            11: 'b',
+            12: 'c',
+            13: 'd',
+            14: 'e',
+            15: 'f',
+        }
+
+        ans = ""
+
+        while num:
+            ans += vals[num % 16]
+            num //= 16
+
+        return ans[::-1]
