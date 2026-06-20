21dbfc8b67f88bcce9c063ac1f262a650dd469c7
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-06-21 03:51:58 +0500

    Time: 0 ms (100%), Space: 19.2 MB (53.48%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -1,52 +1,5 @@
-class Node:
-    def __init__(self, val):
-        self.val = val
-        self.next = None
-
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        n = len(s)
-        if n != len(goal):
+        if len(s) != len(goal):
             return False
-        if (s == "" and goal == "") or s == goal:
-            return True
-
-        # creating a circular linked list for string
-        nodemap = {}
-        start = Node('dummy')
-        prev = start
-        for ch in s:
-            node = Node(ch)
-            prev.next = node
-            prev = node
-            if ch in nodemap:
-                nodemap[ch].append(node)
-            else:
-                nodemap[ch] = [node]
-        prev.next = start.next # removed start/dummy-node automatically
-
-        startCh = goal[0]
-        if startCh not in nodemap:
-            return False
-        for node in nodemap[startCh]:
-            i = 0
-            matched = 0
-            checked = 0
-            while checked < n or matched < n:
-                if goal[i] == node.val:
-                    matched += 1
-                else:
-                    break
-                checked += 1
-                node = node.next
-                if i+1 == n:
-                    i = 0
-                else:
-                    i += 1
-            if matched == n:
-                return True
-            i = 0
-
-        return False
-
-
+        return goal in s+s
