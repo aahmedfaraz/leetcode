a959d743257586c162abbb4125f06c16db03d75c
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-06-22 20:48:14 +0500

    Time: 0 ms (100%), Space: 19.4 MB (20.24%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,4 +1,4 @@
 import math
 class Solution:
     def numTrees(self, n: int) -> int:
-        return int(math.factorial(2 * n) / (math.factorial(n) * math.factorial(n+1)))
+        return math.factorial(2 * n) // (math.factorial(n) * math.factorial(n+1))
