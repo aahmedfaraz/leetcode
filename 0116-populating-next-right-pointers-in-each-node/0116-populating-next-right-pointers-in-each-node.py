a7e03ba9b47437e05ceca8fec9c244d41d3e2ac8
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-07-09 04:09:04 +0500

    Time: 58 ms (56.2%), Space: 20.6 MB (48.33%) - LeetHub

---
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root: return None
+
+        level = [root]
+        
+        while level:
+            newlevel = []
+            for i in range(len(level)):
+                node = level[i]
+                if node.left:
+                    newlevel.append(node.left)
+                if node.right:
+                    newlevel.append(node.right)
+                if i == len(level)-1:
+                    node.next = None
+                else:
+                    node.next = level[i+1]
+            level = newlevel
+        return root
