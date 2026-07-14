eeaf4b499c1b680e116cbc1f8eb752977a09fe8a
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-07-14 15:32:25 +0500

    Time: 1 ms (16.94%), Space: 19.3 MB (70.42%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.py
+++ 0868-binary-gap/0868-binary-gap.py
@@ -1,18 +1,23 @@
 class Solution:
     def binaryGap(self, n: int) -> int:
+        maxval = 0
+        values = bin(n)[2:]
+        left, right = 0, len(values)-1
+
+        # eliminate zeros around the values
+        while values[left] == 0:
+            left += 1
+        while values[right] == 0:
+            right -= 1
+        
+        left += 1 # skip 1st 1 of the window
+
         count = 0
-        larg = 0
-        window = False
-        binnum = bin(n)[2:]
-        for ch in binnum:
-            if ch == '1':
-                if window:
-                    count += 1
-                    larg = max(larg, count)
-                    count = 0
-                else:
-                    window = True
-            else:
-                if window:
-                    count += 1
-        return larg
+        while left <= right:
+            count += 1
+            if values[left] == '1':
+                maxval = max(maxval, count)
+                count = 0
+            left += 1
+        
+        return maxval
