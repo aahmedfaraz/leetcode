226ae8d0214fac139cccf9ae1b7502dba686ee59
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-07-17 00:10:38 +0500

    Time: 87 ms (53.02%), Space: 32.9 MB (77.27%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,29 +1,15 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         rows, cols = len(matrix), len(matrix[0])
-        cache = {}
-        max1sinarea = 0
+        dp = [[0 for _ in range(cols+1)] for _ in range(rows+1)]
 
-        def helper(row, col):
-            nonlocal max1sinarea
-            if row >= rows or col >= cols:
-                return 0
-            if (row, col) in cache:
-                return cache[(row, col)]
-            
-            down = helper(row+1, col)
-            right = helper(row, col+1)
-            diag = helper(row+1, col+1)
+        maxval = 0
 
-            area = 0
-            if matrix[row][col] == '1':
-                area = 1 + min(down, right, diag)
-                max1sinarea = max(max1sinarea, area ** 2)
-                
-            cache[(row, col)] = area
-            return area
+        for row in range(rows-1, -1, -1):
+            for col in range(cols-1, -1, -1):
+                if matrix[row][col] != '0':
+                    dp[row][col] = 1
+                    dp[row][col] += min(dp[row+1][col], dp[row][col+1], dp[row+1][col+1])
+                    maxval = max(maxval, dp[row][col])
         
-        helper(0, 0)
-        return max1sinarea
-# Time = O(rows x cols) = O(n)
-# Space = O(rows x cols) = O(n)
+        return maxval ** 2
