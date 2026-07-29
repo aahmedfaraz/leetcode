e2ec34efef3ed55cc072c6e3e4428df262481cb0
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-07-29 18:10:02 +0500

    Time: 35 ms (89.3%), Space: 34.4 MB (10.94%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,29 +1,47 @@
 class Solution:
-    def isPali(self, s: str) -> bool:
-        left, right = 0, len(s)-1
-        while left < right:
-            if s[left] != s[right]:
-                return False
-            left += 1
-            right -= 1
-        return True
-
     def partition(self, s: str) -> List[List[str]]:
-        que = [[[], s]]
-        subs = []
+        if not s: return []
+        if len(s) == 1: return [[s]]
 
-        while que:
-            prev, rem = que.pop()
+        paliset = set()
 
-            if not rem:
-                subs.append(prev)
-                continue
+        def isPali(subs: str) -> bool:
+            left, right = 0, len(subs)-1
+            while left < right:
+                if subs[left] != subs[right]:
+                    return False
+                left += 1
+                right -= 1
+            return True
 
-            for i in range(len(rem)): 
-                part = rem[:i+1]
-                if self.isPali(part):
-                    que.append([prev+[part], rem[i+1:]])
+        def fillPaliSet(subs: str):
+            for start in range(len(subs)):
+                for cut in range(start+1, len(subs)+1):
+                    if isPali(subs[start: cut]):
+                        paliset.add((start, cut))
 
-        return subs
-# Time = O(n * 2^n)
-# Space = O(n^2)
+        fillPaliSet(s)
+        # print(paliset)
+
+        res = []
+
+        def dfs(path, start, end):
+            # print(path, start, end)
+            if start == end:
+                res.append(path.copy())
+                return
+
+            for cut in range(start+1, end+1):
+                if (start, cut) in paliset:
+                    path.append(s[start: cut])
+                    dfs(path, cut, end)
+                    path.pop()
+
+        dfs([], 0, len(s))
+
+        # print(res)
+
+        return res
+            
+# Time = 
+# Space = 
