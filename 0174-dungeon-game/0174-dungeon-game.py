1c0bd9ccd7f29771a033811fc93b372ec4fae42f
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-08-04 04:51:12 +0500

    Time: 0 ms (100%), Space: 20.1 MB (32.56%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.py
+++ 0174-dungeon-game/0174-dungeon-game.py
@@ -1,16 +1,20 @@
+INF = float('inf')
+
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
         rows, cols = len(dungeon), len(dungeon[0])
 
-        INF = float('inf')
+        if not rows or not cols: return 0
 
+        # create dp
         dp = [INF] * (cols + 1)
-        dp[cols - 1] = 1      # imaginary cell below princess
+        dp[cols - 1] = 1
         dp[cols] = INF
 
-        for row in range(rows - 1, -1, -1):
-            for col in range(cols - 1, -1, -1):
+        # fill dp
+        for row in range(rows-1, -1, -1):
+            for col in range(cols-1, -1, -1):
                 need = min(dp[col], dp[col + 1]) - dungeon[row][col]
                 dp[col] = max(1, need)
-
+        
         return dp[0]
