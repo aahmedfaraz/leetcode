ef7db982b6a8ed4375e5d4ac9fa00b5ed8abf2a0
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-08-04 18:34:29 +0500

    Time: 0 ms (100%), Space: 19.1 MB (86.26%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.py
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.py
@@ -1,17 +1,3 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
-        late = 0
-        absent = 0
-        for att in s:
-            if att == 'A':
-                absent += 1
-                late = 0
-                if absent >= 2:
-                    return False
-            elif att == 'L':
-                late += 1
-                if late >= 3:
-                    return False
-            else:
-                late = 0
-        return True
+        return s.count('A') < 2 and 'LLL' not in s
