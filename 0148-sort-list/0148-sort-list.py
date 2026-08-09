0507f866d394e674ca50946f92fb40e6a39fe067
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-08-09 23:02:06 +0500

    Time: 172 ms (60.4%), Space: 40.7 MB (58.16%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -3,43 +3,47 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return head
 
-        # Find middle
-        slow = head
-        fast = head.next
+        def mergesort(head):
+            if not head or not head.next:
+                return head
+            
+            slow, fast = head, head.next
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+            
+            mid = slow
+            left, right = head, mid.next
+            mid.next = None
+            
+            leftsorted = mergesort(left)
+            rightsorted = mergesort(right)
 
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+            return merge(leftsorted, rightsorted)
 
-        mid = slow.next
-        slow.next = None
+        def merge(head1, head2):
+            dummy = ListNode(0)
+            res = dummy
 
-        # Sort both halves
-        left = self.sortList(head)
-        right = self.sortList(mid)
-
-        # Merge
-        dummy = ListNode()
-        tail = dummy
-
-        while left and right:
-            if left.val <= right.val:
-                tail.next = left
-                left = left.next
+            while head1 and head2:
+                if head1.val <= head2.val:
+                    res.next = head1
+                    head1 = head1.next
+                else:
+                    res.next = head2
+                    head2 = head2.next
+                res = res.next
+            
+            if head1:
+                res.next = head1
             else:
-                tail.next = right
-                right = right.next
-            tail = tail.next
-
-        tail.next = left if left else right
-
-        return dummy.next
-
-# Time = Merge Sort = O(n log n)
-# Space = Recursion = O(log n)
+                res.next = head2
+            
+            return dummy.next
+        
+        return mergesort(head)
