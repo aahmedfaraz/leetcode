365a98064a28e8ec8fc5fe51968e0fd06aced44e
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2026-08-11 23:39:39 +0500

    Time: 0 ms (100%), Space: 28 MB (21.2%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -5,21 +5,40 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        def last_to_second(node):
-            if not node or not node.next:
-                return
-            secondlastnode = None
-            lastnode = node
-            while lastnode.next: # O(n)
-                secondlastnode = lastnode
-                lastnode = lastnode.next
-            second = node.next
-            if second == lastnode:
-                return
-            node.next = lastnode
-            lastnode.next = second
-            secondlastnode.next = None
-            last_to_second(second)
-        last_to_second(head)
-# Time = O(n^2)
-# Space = O(n)
+        if not head or not head.next:
+            return head
+
+        slow, fast = head, head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        l2 = slow.next
+        slow.next = None
+        prev = None
+        while l2:
+            nextnode = l2.next
+            l2.next = prev
+            prev = l2
+            l2 = nextnode
+
+        part1 = head
+        part2 = prev
+
+        curr1 = head
+        curr2 = part2
+
+        while curr1 and curr2:
+            second = curr1.next
+            secondlast = curr2.next
+            
+            curr1.next = curr2
+            curr2.next = second
+
+            curr1 = second
+            curr2 = secondlast
+
+# head = ListNode(1, ListNode(2, ListNode(3, ListNode(4))))
+
+# Solution.reorderlist(head)
